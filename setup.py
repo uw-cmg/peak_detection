ce6aebbbef345203d3de9fe04a9b3dc83912aefc
wdwzyyg <jwei74@wisc.edu>
2024-08-14 01:03:56 -0500

    minor change

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     # description='',
     # keywords=[],
     install_requires=[
-        "torch=2.2.2",
-        "torchvision=0.16.2",
+        "torch==2.2.2",
+        "torchvision==0.16.2",
         "numpy>=1.24.1",
         "matplotlib",
         "scikit-image",
